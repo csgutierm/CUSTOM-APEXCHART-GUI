bb91cc18d1910193a75dfc32a7fd3d03269ab7ea
csgutierm <csgutier@gmail.com>
2023-06-26 23:10:58 -0400

    first commit

---
+++ src/custom.js
@@ -0,0 +1,290 @@
+
+//Inicializaciones
+window.onload=check;
+function check() {
+  document.getElementById("radioDefault").checked = true;
+
+document.getElementById("inputTitulo").value = tituloCUSTOM;
+//document.getElementById("inputTituloFontSize").value = tituloFontSizeCUSTOM;
+document.getElementById("inputTituloFontSize").value = 22;
+
+
+var selectElement = document.getElementById('inputTituloFontFamily');
+var defaultValue = 'sans-serif'; // Set the default value here
+
+selectElement.value = defaultValue;
+
+tituloFontFamilyCUSTOM = defaultValue;
+  
+}
+
+//Utilidad
+function onlyNumbers(event) {
+    const key = event.key;
+    const currentValue = event.target.value + key;
+    const isValid = /^\b([1-9]|[1-8][0-9]|9[0-6])\b/.test(currentValue); 
+    return isValid;
+  }
+
+
+//Datos compartidos
+
+//valor inicial 
+chartType = 'bar';
+
+let datosCUSTOM = [44, 55, 41, 17, 15];
+let categoriasCUSTOM = ['Feb', 'Mar', 'Abr', 'May', 'Jun'];
+let alturaCUSTOM = 350;
+
+tituloCUSTOM = 'Ventas últimos 5 meses';
+//document.getElementById("inputTitulo").placeholder = tituloCUSTOM;
+
+tituloFontSizeCUSTOM = 22 //14 es el valor default que trae apexcharts al parecer
+//document.getElementById("inputTituloFontSize").value = tituloFontSizeCUSTOM;
+
+tituloFontWeightCUSTOM = 900 //900 es el valor default que trae apexcharts al parecer
+//document.getElementById("inputTituloFontWeight").value = tituloFontWeightCUSTOM;
+
+tituloFontFamilyCUSTOM = ''
+//document.getElementById("inputTituloFontFamily").value = tituloFontFamilyCUSTOM;
+
+
+let nombreSerieCUSTOM = 'Ventas';
+
+// Datos del gráfico de barras
+var barOptions = {
+  series: [{
+    name: nombreSerieCUSTOM,
+    data: datosCUSTOM
+  }],
+  chart: {
+    type: 'bar',
+    height: alturaCUSTOM
+  },
+  labels: categoriasCUSTOM,
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val
+    }
+  },
+  title: {
+    text: tituloCUSTOM,
+    align: 'left',
+    style: {
+        fontSize:  tituloFontSizeCUSTOM,
+    }
+  },
+};
+
+var lineOptions = {
+  series: [{
+    name: nombreSerieCUSTOM,
+    data: datosCUSTOM
+  }],
+  chart: {
+    height: alturaCUSTOM,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: tituloCUSTOM,
+    align: 'left',
+    style: {
+        fontSize:  tituloFontSizeCUSTOM,
+    }
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  xaxis: {
+    categories: categoriasCUSTOM,
+  }
+};
+
+// Configuración del gráfico de donut
+var donutOptions = {
+  series: datosCUSTOM, //no es compatible con name:
+  chart: {
+    type: 'donut',
+    height: alturaCUSTOM
+  },
+  labels: categoriasCUSTOM,
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val.toFixed(1) + "%"
+    },
+    style: {
+      fontSize: '16px',
+      fontFamily: 'Helvetica, Arial, sans-serif',
+      fontWeight: 'bold',
+      colors: ['#123456']
+    },
+  },
+  title: {
+    text: tituloCUSTOM,
+    align: 'left',
+    style: {
+        fontSize:  tituloFontSizeCUSTOM,
+    }
+  },
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), barOptions);
+chart.render();
+
+// Función para cambiar entre el gráfico de barras y el gráfico de donut
+function changeChartType(type) {
+
+    chartType = type;
+
+  //console.log(chart)
+  if (type === 'donut') {
+    chart.destroy();
+    console.log(donutOptions);
+
+    donutOptions.title.text = tituloCUSTOM;
+
+    donutOptions.title.style = {
+        fontSize:  tituloFontSizeCUSTOM,
+        fontWeight:  tituloFontWeightCUSTOM, //'bold',
+        fontFamily:  tituloFontFamilyCUSTOM,
+        //color:  '#263238'
+      };
+    
+    chart = new ApexCharts(document.querySelector("#chart"), donutOptions);
+    chart.render();
+  }
+
+  if (type === 'bar') {
+    chart.destroy();
+    barOptions.title.text = tituloCUSTOM;
+
+    barOptions.title.style = {
+        fontSize:  tituloFontSizeCUSTOM,
+        fontWeight:  tituloFontWeightCUSTOM, //'bold',
+        fontFamily:  tituloFontFamilyCUSTOM,
+        //color:  '#263238'
+      };
+
+    chart = new ApexCharts(document.querySelector("#chart"), barOptions);
+    chart.render();
+  }
+
+  if (type === 'line') {
+    chart.destroy();
+    lineOptions.title.text = tituloCUSTOM;
+
+    lineOptions.title.style = {
+        fontSize:  tituloFontSizeCUSTOM,
+        fontWeight:  tituloFontWeightCUSTOM, //'bold',
+        fontFamily:  tituloFontFamilyCUSTOM,
+        //color:  '#263238'
+      };
+
+    chart = new ApexCharts(document.querySelector("#chart"), lineOptions);
+    chart.render();
+  }
+
+}
+
+
+function cambiarTitulo() {
+    var valor = document.getElementById("inputTitulo").value; // Obtener el valor del campo de entrada
+
+    tituloCUSTOM = valor;
+    tituloFontSizeCUSTOM = document.getElementById("inputTituloFontSize").value;
+
+    if (chartType === 'donut') {
+        changeChartType('donut');
+    }
+
+    if (chartType === 'bar') {
+        changeChartType('bar');
+    }
+
+    if (chartType === 'line') {
+        changeChartType('line');
+    }
+    
+}
+
+
+function cambiarTituloFontSize() {
+    var valor = document.getElementById("inputTituloFontSize").value; // Obtener el valor del campo de entrada
+
+    tituloFontSizeCUSTOM = valor;
+
+    if (chartType === 'donut') {
+        changeChartType('donut');
+    }
+
+    if (chartType === 'bar') {
+        changeChartType('bar');
+    }
+
+    if (chartType === 'line') {
+        changeChartType('line');
+    }
+    
+}
+
+function cambiarTituloFontWeight(event) {
+
+    const inputValue = event.target.value;
+    const validValues = ['100', '200', '300', '400', '500', '600', '700', '800', '900'];
+
+    if (!validValues.includes(inputValue)) {
+        inputValue = '500'; // Change the input value if it's not a valid value
+    }
+
+    tituloFontWeightCUSTOM = inputValue;
+
+    if (chartType === 'donut') {
+        changeChartType('donut');
+    }
+
+    if (chartType === 'bar') {
+        changeChartType('bar');
+    }
+
+    if (chartType === 'line') {
+        changeChartType('line');
+    }
+
+}
+
+
+function cambiarTituloFontFamily(event) {
+
+    const inputValue = event.target.value;
+
+
+
+    tituloFontFamilyCUSTOM = inputValue;
+
+    if (chartType === 'donut') {
+        changeChartType('donut');
+    }
+
+    if (chartType === 'bar') {
+        changeChartType('bar');
+    }
+
+    if (chartType === 'line') {
+        changeChartType('line');
+    }
+
+}
